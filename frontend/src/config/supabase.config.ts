275e62ff933ca23b8b11a7eb601e48153c73d22a
zdebar <157598603+zdebar@users.noreply.github.com>
2025-10-30 16:34:44 +0100

    feat: Default Supabase Auth with Email

--- frontend/src/config/supabase.config.ts
+++ frontend/src/config/supabase.config.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY!;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
