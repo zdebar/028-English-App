298f28578425d4a4e8142bb7f3b786cee803676e
zdebar <157598603+zdebar@users.noreply.github.com>
2026-01-06 17:28:16 +0100

    fix: adjust footer text size and clean up CSS for improved readability

--- frontend/src/components/Layout/footer.tsx
+++ frontend/src/components/Layout/footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-4 text-center flex gap-2 justify-center items-center">
-      <p className="text-sm">&copy; {currentYear}</p>
-      <a href="/privacy-policy" className="text-sm hover:underline">
+    <footer className="w-full text-sm py-4 text-center flex gap-2 justify-center items-center">
+      <p>&copy; {currentYear}</p>
+      <a href="/privacy-policy" className="hover:underline">
         Zásady ochrany osobních údajů
       </a>
     </footer>
