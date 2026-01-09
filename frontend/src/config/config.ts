7f7e84f804a04f4fe6f23cae6dc99efe3855988c
zdebar <157598603+zdebar@users.noreply.github.com>
2026-01-09 18:41:23 +0100

    fix: update deck size in lesson configuration from 5 to 8

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -38,7 +38,7 @@ const config = {
   },
 
   lesson: {
-    deckSize: 5, // Number of items per deck
+    deckSize: 8, // Number of items per deck
     lessonSize: 100, // Number of items per lesson
   },
 
