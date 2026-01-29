af05ddb10924040cf1c3559e6147cfcc65846f42
zdebar <zdebarth@gmail.com>
2026-01-29 08:29:29 +0100

    refactor: simplify Footer component by removing currentYear prop and defining it internally

--- frontend/src/components/Layout/footer.tsx
+++ frontend/src/components/Layout/footer.tsx
@@ -1,15 +1,13 @@
 import PrivacyPolicyLink from '@/features/privacy-policy/PrivacyPolicyLink';
 
+const currentYear = new Date().getFullYear();
+
 /**
  * Footer component that displays the current year copyright and a link to the privacy policy.
  *
  * @param currentYear - The current year to display in the footer. Defaults to the current year.
  */
-export default function Footer({
-  currentYear = new Date().getFullYear(),
-}: {
-  currentYear?: number;
-}) {
+export default function Footer() {
   return (
     <footer className="flex w-full items-center justify-center gap-2 py-4 text-center text-sm">
       <p>© {currentYear}</p>
