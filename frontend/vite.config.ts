2036f57abb4607e1515e25356224ec8e761dc906
zdebar <zdebarth@gmail.com>
2026-02-23 17:50:04 +0100

    chore: install React Compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,14 @@ import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
