b81e0beb0a888ad1de3ac087ade6c0d15d7dfc76
zdebar <zdebarth@gmail.com>
2026-02-27 09:52:30 +0100

    fix: improved description and UI of OverviewCard

--- frontend/src/components/UI/OverviewCard.tsx
+++ frontend/src/components/UI/OverviewCard.tsx
@@ -17,7 +17,8 @@ interface OverviewCardProps {
 
 /**
  * OverviewCard component for displaying a card with a title, and content.
- * Title button triggers confirmation modal for handleReset function. If handleResest is not provided, the reset button is disabled.
+ * Title button triggers confirmation modal for handleReset function.
+ * If handleReset is not provided, the reset button is disabled.
  * Component doesn't handle errors. Errors should be handled in the parent component.
  *
  * @param buttonTitle Text to display on the button.
@@ -27,7 +28,7 @@ interface OverviewCardProps {
  * @param handleReset Function to call to reset progress.
  * @param onClose Function to call when closing the card.
  * @param className Additional CSS classes for custom styling.
- * @param children Content to be displayed inside the button.
+ * @param children Content to be displayed inside the content area.
  * @returns The rendered OverviewCard component.
  */
 export default function OverviewCard({
@@ -43,7 +44,7 @@ export default function OverviewCard({
   return (
     <div className={`card-width ${className}`}>
       {/* Top Bar */}
-      <div className="relative flex items-center justify-between gap-1 pb-1">
+      <div className="relative flex items-center justify-between gap-1">
         {/* Title and Reset Button */}
         <ButtonWithModal
           modalTitle={modalTitle}
