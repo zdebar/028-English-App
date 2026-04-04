1d3ba7bf564bc5aa43f6ab8bfef778bded80cd1c
zdebar <zdebarth@gmail.com>
2026-04-04 10:40:29 +0200

    feat: refine Guide component text for clarity and conciseness

--- frontend/src/pages/Guide.tsx
+++ frontend/src/pages/Guide.tsx
@@ -15,58 +15,28 @@ export default function Guide(): JSX.Element {
           Běžné učení jazyků používá příliš mnoho teori a málo praxe. Tato aplikace je naopak
           zaměřená na dril, na velké množství naposlouchaných a vyslovených opakování.
         </p>
-        <p className="text-center">
-          Zatímco běžně během sezení hodiny angličtiny namluvíte přinejlepším pár desítek slovíček,
-          zde jich za stejnou dobu zvládnete procvičit stovky.
-        </p>
       </section>
       <section>
         <h2>Používání</h2>
         <p className="text-center">
           Klikněte na tlačítko "Procvičovat" na horní liště a začněte procvičovat. Aplikace vám bude
-          nabízet slovíčka, slovní spojení či věty v ideálním pořadí pro učení.
-        </p>
-        <p className="text-center">
-          Pravidelně se střídá procvičování z češtiny do angličtiny a z angličtiny do češtiny. Z
-          češtiny psaným slovíčkem, z angličtiny poslechem.
-        </p>
-        <p className="text-center">
-          Každý den se počítá doporučený minimální denní cíl opakování. Najdete ho jak na titulní
-          stránce tak na kartičce procvičování.
+          nabízet slovíčka, slovní spojení či věty v ideálním pořadí pro učení. 
         </p>
       </section>
       <section>
         <h2>Neodhalená kartička</h2>
         <Screenshot src="/screenshots/not-revealed.webp" alt="Ukázka kartičky před odhalením" />
         <p>
-          <strong>"odhalit"</strong>
-          <span>Odhalí překlad kartičky</span>
-        </p>
-
-        <p>
-          <strong>"pokrok"</strong>
-          <span>Skóre pokroku položky</span>
-        </p>
-        <p>
           <strong>"dnes / denní cíl"</strong>
-          <span>Dnešní počet opakování a cíl</span>
+          <span>Doporučenný denní počet opakování. Snažte se ho každý den splnit. Vše ostatní můžete pustit z hlavy.</span>
         </p>
         <p>
           <strong>"dokončit"</strong>
-          <span>Položka se nebude nabízet k dalšímu procvičování</span>
+          <span>Položka se nebude nabízet k dalšímu procvičování. Použijte k přeskočení položky, pokud ji již znáte z dřívějška.</span>
         </p>
-        <p>
-          <strong>"přehraj audio"</strong>
-          <span>Přehraje audio položky</span>
-        </p>
-
         <p>
           <strong>"gramatika"</strong>
-          <span>Vysvětlení příslušné gramatiky</span>
-        </p>
-        <p>
-          <strong>"nápověda"</strong>
-          <span>Postupně odhalí jednotlivá písmena správného překladu</span>
+          <span>Vysvětlení příslušné gramatiky ke každé větě či slovnímu spojení.</span>
         </p>
       </section>
       <section>
@@ -74,17 +44,13 @@ export default function Guide(): JSX.Element {
         <Screenshot src="/screenshots/revealed.webp" alt="Ukázka kartičky po odhalení" />
         <p>
           <strong>"neznám"</strong>
-          <span>Sníží skóre dané položky. Položka se nabídne k dalšímu procvičování dříve.</span>
+          <span>Sníží skóre dané položky. Položka se nabídne k dalšímu procvičování dříve. Pokud pro vás není znalost zcela automatická, dávejte vždy "neznám".</span>
         </p>
         <p>
           <strong>"znám"</strong>
           <span>
             Zvýší skóre dané položky. Položka se bude tak nabízet k dalšímu procvičování později.
           </span>
-        </p>
-        <p>
-          <strong>"DOPORUČUJEME"</strong>
-          <span>Dávat "znám" jen pokud je pro vás znalost zcela automatická!</span>
         </p>
       </section>
       <section>
