5a5048f608b2b3219fd8c607b25ebdcd85b780fc
zdebar <zdebarth@gmail.com>
2026-04-20 15:06:19 +0200

    fix: improve formatting of dangerouslySetInnerHTML in GrammarCard for consistency

--- frontend/src/features/practice/GrammarCard.tsx
+++ frontend/src/features/practice/GrammarCard.tsx
@@ -25,10 +25,8 @@ export default function GrammarCard({ grammar, onClose }: GrammarCardProps) {
     <OverviewCard buttonTitle={grammar?.name} onClose={onClose}>
       {grammar?.note ? (
         <div
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(grammar.note),
-          }}
           className="grammar"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(grammar.note) }}
         />
       ) : (
         TEXTS.noNotesToDisplay
