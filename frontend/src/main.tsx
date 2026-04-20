42db6cfa8bcd38adf7ff1f49dbc66a9fd72dedca
zdebar <zdebarth@gmail.com>
2026-04-20 17:13:30 +0200

    fix: restore service worker registration logic in main.tsx for improved app functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js');
-//     navigator.serviceWorker.addEventListener('message', (event) => {
-//       if (event.data && event.data.type === 'refresh') {
-//         window.location.reload();
-//       }
-//     });
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.addEventListener('message', (event) => {
+      if (event.data?.type === 'refresh') {
+        globalThis.location.reload();
+      }
+    });
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
