0dfc3a043e79ee451f8d486134c807372326b9d9
zdebar <zdebarth@gmail.com>
2026-05-01 13:05:06 +0200

    refactor: simplify SyncButton logic and integrate success/error toast text props

--- frontend/src/features/sync/SyncButton.tsx
+++ frontend/src/features/sync/SyncButton.tsx
@@ -1,8 +1,6 @@
 import { useAuthStore } from '@/features/auth/use-auth-store';
-import { useToastStore } from '@/features/toast/use-toast-store';
 import { TEXTS } from '@/locales/cs';
 import { type JSX } from 'react';
-import { errorHandler } from '../logging/error-handler';
 import { audioSync, dataSync } from '@/database/utils/data-sync.utils';
 import { logRejectedResults } from '../logging/logging.utils';
 import ModalButton from '../modal/ModalButton';
@@ -19,36 +17,26 @@ type SyncButtonProps = Readonly<{
  */
 export default function SyncButton({ className }: SyncButtonProps): JSX.Element {
   const userId = useAuthStore((state) => state.userId);
-  const showToast = useToastStore((state) => state.showToast);
-  const hideToast = useToastStore((state) => state.hideToast);
 
   const handleSync = async () => {
     if (!userId) return;
-
-    try {
-      showToast(TEXTS.syncLoadingText, 'info', true);
-      const userResults = await Promise.allSettled([dataSync(userId, true), audioSync(userId)]);
-      const isError = logRejectedResults(userResults, 'Data synchronization error:');
-      if (isError) throw new Error('Data synchronization error');
-      hideToast();
-      showToast(TEXTS.syncSuccessToast, 'success');
-    } catch (error) {
-      hideToast();
-      showToast(TEXTS.syncErrorToast, 'error');
-      errorHandler('Sync Error', error);
-    }
+    const userResults = await Promise.allSettled([dataSync(userId, true), audioSync(userId)]);
+    const isError = logRejectedResults(userResults, 'Data synchronization error:');
+    if (isError) throw new Error('Data synchronization error');
   };
 
   return (
     <ModalButton
       onConfirm={handleSync}
       className={className}
+      successToastText={TEXTS.syncSuccessToast}
+      errorToastText={TEXTS.syncErrorToast}
       disabled={!userId}
       title={TEXTS.dataSyncTooltip}
       modalTitle={TEXTS.syncButton}
       modalText={TEXTS.syncButtonDescription}
     >
-      <p className="mx-auto w-40">{TEXTS.syncButton}</p>
+      {TEXTS.syncButton}
     </ModalButton>
   );
 }
