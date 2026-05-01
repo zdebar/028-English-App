69a06b3a17ae4961052267ef00f64c9c8bc18a7f
zdebar <zdebarth@gmail.com>
2026-05-01 11:17:58 +0200

    refactor: simplify InstallPWAButton props and improve type handling

--- frontend/src/features/pwa/InstallPwaButton.tsx
+++ frontend/src/features/pwa/InstallPwaButton.tsx
@@ -6,10 +6,9 @@ import { usePwaStore } from './use-pwa-store';
 
 type InstallPWAButtonProps = Readonly<{
   className?: string;
-}> &
-  React.HTMLAttributes<HTMLParagraphElement>;
+}>;
 
-export function InstallPWAButton(props: InstallPWAButtonProps) {
+export function InstallPWAButton({ className }: InstallPWAButtonProps) {
   const promptEvent = usePwaStore((state) => state.promptEvent);
   const setPromptEvent = usePwaStore((state) => state.setPromptEvent);
   const clearPromptEvent = usePwaStore((state) => state.clearPromptEvent);
@@ -36,10 +35,9 @@ export function InstallPWAButton(props: InstallPWAButtonProps) {
 
   return (
     <Notification
-      {...props}
       title={TEXTS.installButtonTooltip}
       onClick={handleInstall}
-      className={`color-link cursor-pointer ${props.className}`}
+      className={`color-link cursor-pointer ${className}`}
     >
       {TEXTS.installButton}
     </Notification>
