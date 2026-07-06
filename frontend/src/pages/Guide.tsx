81d46ca1800f6aa1853273817acecfdb26056c9a
zdebar <zdebarth@gmail.com>
2026-07-06 16:02:53 +0200

    refactor: update Guide

--- frontend/src/pages/Guide.tsx
+++ frontend/src/pages/Guide.tsx
@@ -13,48 +13,59 @@ export default function Guide(): JSX.Element {
     <div className="guide card-width overflow-x-hidden">
       <h1 className="text-center">Návod</h1>
       <section>
-        <p className="text-center">
-          Tato učební aplikace je zaměřena na dril, na velké množství naposlouchaných a vyslovených
-          opakování.
+        <h2>Představení</h2>
+        <p>
+          Tato učební aplikace je zaměřena na velké množství naposlouchaných a vyslovených
+          opakování. Není třeba přemýšlet co dělat dále, aplikace vždy sama zvolí procvičování či
+          učení nového.
         </p>
       </section>
       <section>
-        <h2>Používání</h2>
-        <p className="text-center">
-          Klikněte na tlačítko "Procvičovat" na horní liště a začněte procvičovat. Aplikace vám bude
-          nabízet slovíčka, slovní spojení či věty v ideálním pořadí pro učení.
+        <h2>Domácí stránka</h2>
+        <Screenshot
+          src={`${import.meta.env.BASE_URL}screenshots/mobile`}
+          alt="Domácí stránka aplikace"
+        />
+        <h3>Nainstalovat aplikaci</h3>
+        <p>
+          Umožňuje nainstalovat aplikaci na plochu vašeho zařízení. Aplikace je plně funkční
+          offline.
+        </p>
+        <h3>Denní cíl</h3>
+        <p>Doporučený minimální denní cíl je 10 hvězdiček, tedy 400 procvičovaných položek.</p>
+        <h3>Slovíčka</h3>
+        <p>Základní učení a procvičování slovíček. Tlačítko je vždy k dispozici.</p>
+        <h3>Nová gramatika</h3>
+        <p>
+          Základní učení a procvičování nové gramatiky. Tlačítko je k dispozici po rozučení všech
+          slovíček souvisejících s danou lekcí.
+        </p>
+        <h3>Gramatika</h3>
+        <p>
+          Následné procvičování rozučené gramatiky. Tlačítko je k dispozici pokud je co procvičovat.
+        </p>
+        <h3>Přehled lekcí</h3>
+        <p>
+          Přehled dnes dotčených lekcí. Lze přepínat mezi rozučenými a naučenými položkami. Položky
+          se určí jako naučené zhruba po 3 měsících.
         </p>
       </section>
-      <section>
-        <h2>Doporučujeme</h2>
-        <ul className="text-left">
-          <li>každé slovíčko či větu nahlas zopakovat</li>
-          <li>preferovat "Opakovat" před "Znám", pokud není znalost zcela automatické</li>
-          <li>procvičit každý den min. 400 opakování tedy 10 hvězdiček</li>
-        </ul>
-      </section>
-
       <section>
         <h2>Neodhalená kartička</h2>
         <Screenshot
           src={`${import.meta.env.BASE_URL}screenshots/not-revealed`}
           alt="Ukázka kartičky před odhalením"
         />
+        <h3>Pokrok</h3>
+        <p>Učební skóre dané položky.</p>
         <h3>Denní pokrok</h3>
-        <p>
-          <span>10 bronzových hvězd po 40 opakováních je doporučené minimální procvičování.</span>
-        </p>
-        <h3>Dokončit</h3>
-        <p>
-          <span>
-            Položka se označí jako naučená. Použijte k přeskočení položky, pokud ji již znáte z
-            dřívějška.
-          </span>
-        </p>
+        <p>Zobrazuje počet dosažených denních hvězdiček a číselný postup na hvězdičce.</p>
+        <h3>Nápověda</h3>
+        <p>Písmeno po písmeně odhaluje slovíčko či větu.</p>
         <h3>Gramatika</h3>
-        <p>
-          <span>Vysvětlení příslušné gramatiky.</span>
-        </p>
+        <p>Zobrazí vysvětlení příslušné gramatiky.</p>
+        <h3>Poznámka</h3>
+        <p>Zobrazí dodatečné informace k položce.</p>
       </section>
       <section>
         <h2>Odhalená kartička</h2>
@@ -62,21 +73,12 @@ export default function Guide(): JSX.Element {
           src={`${import.meta.env.BASE_URL}screenshots/revealed`}
           alt="Ukázka kartičky po odhalení"
         />
+        <h3>Dokončit</h3>
+        <p>Označí danou položku jako naučenou. Položka se dále nebude nabízet k procvičování.</p>
         <h3>Opakovat</h3>
-        <p>
-          <span>Položka se nabídne k dalšímu procvičování dříve.</span>
-        </p>
+        <p>Položka se nabídne k dalšímu procvičování dříve.</p>
         <h3>Znám</h3>
-        <p>
-          <span>Položka se nabídne k dalšímu procvičování později.</span>
-        </p>
-        <h3>Poznámka</h3>
-        <p>
-          <span>
-            Některé položky mají poznámku, která podrobněji vysvětluje použití daného slovíčka v
-            angličtině.
-          </span>
-        </p>
+        <p>Položka se nabídne k dalšímu procvičování později.</p>
         <h3>Zkratky</h3>
         <p>
           <span className="inline-block w-20">zkr.</span>zkrácená varianta
@@ -89,57 +91,51 @@ export default function Guide(): JSX.Element {
         </p>
       </section>
       <section>
+        <h2>Doporučujeme</h2>
+        <ul className="text-left">
+          <li>Každé slovíčko či větu nahlas zopakovat</li>
+          <li>Preferovat "Opakovat" před "Znám", pokud není znalost zcela automatické</li>
+          <li>Procvičit každý den min. 400 opakování tedy 10 hvězdiček</li>
+        </ul>
+      </section>
+      <section>
         <h2>Profil</h2>
         <Screenshot
           src={`${import.meta.env.BASE_URL}screenshots/profile`}
           alt="Ukázka profilu uživatele"
         />
+        <h3>Přehled CEFR úrovní</h3>
+        <p>Přehled postupu na jednotlivých CEFR úrovních a na jednotlivých lekcích.</p>
+        <h3>Přehled gramatiky</h3>
+        <p>Přehled započaté gramatiky.</p>
+        <h3>Přehled témat</h3>
+        <p>Přehled započatých tematických skupin slovíček např. dny v týdnu, měsíce atd.</p>
+        <h3>Přehled slovíček</h3>
+        <p>Přehled započatých slovíček.</p>
+        <h3>Smazat účet</h3>
         <p>
-          <strong>"Přehled CEFR úrovní"</strong>
-          <span>Přehled postupu na jednotlivých CEFR úrovních a na jednotlivých lekcích.</span>
+          Vaše údaje budou uchovány po dalších 30 dní, kdy je možné účet obnovit opětovným
+          přihlášením. Po 30 dnech je účet nenávratně smazán.
         </p>
-        <p>
-          <strong>"Přehled gramatiky"</strong>
-          <span>
-            Přehled již započaté gramatiky umožňuje restartovat procvičování vět příslušných k dané
-            gramatice.
-          </span>
-        </p>
-        <p>
-          <strong>"Přehled slovíček"</strong>
-          <span>
-            Přehled započatých slovíček umožňuje restartovat procvičování každého slovíčka.
-          </span>
-        </p>
-        <p>
-          <strong>"Přehled témat"</strong>
-          <span>Přehled započatých tematických skupin slovíček např. dny v týdnu, měsíce atd.</span>
-        </p>
-        <p>
-          <strong>"Smazat účet"</strong>
-          <span>
-            Smaže uživatelský účet včetně všech vašich dat. Smazání účtu nevratné a data budou
-            nenávratně odstraněna.
-          </span>
-        </p>
-        <p>
-          <strong>"Odhlásit se"</strong>
-          <span>Odhlásí vás z vašeho uživatelského účtu.</span>
-        </p>
+        <h3>Odhlásit se</h3>
+        <p>Odhlásí vás z vašeho uživatelského účtu.</p>
       </section>
       <section>
         <h2>Offline</h2>
-        <p className="text-center">
+        <p>
           Aplikace je plně funkční offline. Data se ukládají do prohlížeče a synchronizují se s
           cloudem, když jste online.
         </p>
       </section>
       <section>
         <h2>Synchronizace</h2>
-        <p className="text-center">
+        <p>
           Data se synchronizují pouze při startu aplikace, či jednou denně, pokud aplikaci necháváte
-          otevřenou. Vzhledem k méně častému synchronizování není aplikace vhodná pro současné
-          používání na více zařízeních.
+          otevřenou.
+        </p>
+        <p>
+          Vzhledem k méně častému synchronizování není aplikace vhodná pro současné používání na
+          více zařízeních.
         </p>
       </section>
     </div>
