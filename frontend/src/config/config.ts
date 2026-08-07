b09ededdc07ad7bf9d99c0c41d70f6c2850a6677
zdebar <zdebarth@gmail.com>
2026-08-07 07:35:23 +0200

    refactor: change enToCz intervals array

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -25,7 +25,7 @@ const config = {
     // Spaced Repetition System configuration
     intervals: {
       czToEn: [0, 120, 900, 3600, 14400, 86400, 172800, 345600, 691200],
-      enToCz: [0, 120, 900, 3600, 14400, 86400, 172800, 345600, 691200],
+      enToCz: [60, 450, 1800, 7200, 43200, 128000, 260000, 520000],
     },
     randomness: 0.2, // Randomness of SRS algorithm
   },
