00852238f78865061386c76fc7bec3830c3347f5
zdebar <zdebarth@gmail.com>
2026-09-10 06:55:22 +0200

    refactor: empty enToCZ SRS interval

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -24,7 +24,7 @@ const config = {
     // Spaced Repetition System configuration
     intervals: {
       czToEn: [15, 150, 14400, 43200, 86400, 172800, 345600, 691200, 1382400, 2764800],
-      enToCz: [300, 43200, 172800, 691200, 2764800],
+      enToCz: [],
     },
     randomness: 0.2, // Randomness of SRS algorithm
   },
