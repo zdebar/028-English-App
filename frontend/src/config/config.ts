43d9a7d12f249b4ba7fb642e8573b61b66ecab70
zdebar <zdebarth@gmail.com>
2026-09-26 10:11:58 +0200

    refactor: set reviewMinimumSize to 48

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -32,7 +32,7 @@ const config = {
 
   practice: {
     initialTrainingBatchSize: 8, // Maximum size of an automatically assembled initial-training batch
-    reviewMinimumSize: 16, // Minimum number of due items required to start a review batch
+    reviewMinimumSize: 48, // Minimum number of due items required to start a review batch
     dailyStartedGoal: 48, // Daily started-item goal shown on the home page
     audioDelay: 100, // Delay in milliseconds for automatically playing audio
     holdDuration: 300, // Duration in milliseconds for which the practice card is held before moving to the next item
